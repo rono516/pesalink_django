c1370aab92466c8a74a65be3b526afd3c50ba6af
Collins Kiplangat Rono <ronocollins2000@gmail.com>
2024-07-12 15:29:09 +0300

    initial commit

---
+++ api/views.py
@@ -0,0 +1,58 @@
+from django.http import JsonResponse
+import requests
+
+client_id = "65ddb2bd197476634ded6d82ed5ed7fd"
+client_secret = "870f83bcd5a17167c0f642000e8c4346"
+token_url = "https://api.connect.stanbicbank.co.ke/api/sandbox/auth/oauth2/token"
+scope = "payments"
+def index(request):
+    payload = {
+    'grant_type': 'client_credentials',
+    'client_id': client_id,
+    'client_secret': client_secret,
+    'scope': scope
+    }
+    response = requests.post(token_url,data=payload)
+    return JsonResponse(response.json())
+
+
+{
+  "originatorAccount": {
+    "identification": {
+      "mobileNumber": "254737696956"
+    }
+  },
+  "requestedExecutionDate": "2021-10-27",
+  "sendMoneyTo": "ACCOUNT.NUMBER",
+  "dbsReferenceId": "98989271771176942",
+  "txnNarrative": "TESTPESALINK",
+  "callBackUrl": "https://clientdomain.com/client/Callback",
+  "transferTransactionInformation": {
+    "instructedAmount": {
+      "amount": "500",
+      "currencyCode": "KES"
+    },
+    "counterpartyAccount": {
+      "identification": {
+        "recipientMobileNo": "254792009556",
+        "recipientBankAcctNo": "01008747142",
+        "recipientBankCode": "07000"
+      }
+    },
+    "counterparty": {
+      "name": "HEZBON",
+      "postalAddress": {
+        "addressLine": "KENYA",
+        "postCode": "1100 ZZ",
+        "town": "Nairobi",
+        "country": "KE"
+      }
+    },
+    "remittanceInformation": {
+      "type": "FEES PAYMENTS",
+      "content": "SALARY"
+    },
+    "endToEndIdentification": "5e1a3da132cc"
+  }
+}
+
